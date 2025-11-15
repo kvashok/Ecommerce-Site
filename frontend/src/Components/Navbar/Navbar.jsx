0ec1872b57a25a00118fb7c6f77fa325f3e7b625
kvashok <kvashok1994@gmail.com>
2025-11-16 00:00:56 +0530

    Added images

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import './Navbar.css';
+import logo from '../../Components/Assets/logo.png';
+import cart from '../../Components/Assets/cart_icon.png';
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <div className="nav-logo">
+            <img src={logo} alt="logo" />
+            <p>SHOPPER</p>
+        </div>
+        <ul className="nav-menu">
+            <li>Shop</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>Kids</li>
+        </ul>
+        <div className="nav-login-cart">
+            <button className="login-btn">Login</button>
+            <img src={cart} alt="cart" />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
