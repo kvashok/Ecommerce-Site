f404db7f8d5261b4acae0e5eacf36803382abab6
kvashok <kvashok1994@gmail.com>
2025-11-16 15:41:32 +0530

    Added loading image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,14 @@
 import Navbars from './Components/Navbar/Navbar'
-
+import Loading from "./Components/Loading/Loading.jsx";
+import { useEffect, useState } from "react";
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3300);
+  }, []);
+  if (loading) {
+    return <Loading />;
+  }
   return (
     <>
       <Navbars />
