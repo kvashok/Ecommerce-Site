3c75638adbf5e96bcd0910878872ee72c96040c5
kvashok <kvashok1994@gmail.com>
2025-11-16 12:06:41 +0530

    changed logo title

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div className="navbar">
         <div className="nav-logo">
             <img src={logo} alt="logo" />
-            <p>SHOPPER</p>
+            <p>SHOPPERS Destination</p>
         </div>
         <ul className="nav-menu">
             <li>Shop</li>
